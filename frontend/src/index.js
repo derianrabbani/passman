9e4fa0c16cd88c67836f5df5b0da2d7d1f3da237
Derian Rabbani <derian.rabbani@gmail.com>
2021-04-08 14:27:57 +0700

    Set CI via package.json

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App"
+// require("dotenv").config()
+
+// process.env.CI = false
 
 ReactDOM.render(<App />, document.querySelector("#root"))
